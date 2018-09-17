881a69a9cc556f1e9b057ccc5bd4c56dd3416b87
Ilya Orson <ilya.orson@gmail.com>
2018-09-16 22:05:22 -0500

    define ode system model just once

--- integrator.py
+++ integrator.py
@@ -2,6 +2,17 @@ import numpy as np
 import scipy.integrate as scp
 
 
+def state_model(t, state, f_args):
+
+    U, a, b = f_args
+    y1, y2 = state
+
+    dev_y1 = -(U + U**2 * a) * y1
+    dev_y2 = U * y1 * b
+
+    return [dev_y1, dev_y2]
+
+
 def model_integration(params, initial_state, controls, time_interval):
     '''
     params: dictionary of parameters passed to a model
@@ -14,16 +25,9 @@ def model_integration(params, initial_state, controls, time_interval):
     a = params['a']
     b = params['b']
 
-    def state_model(t, initial_state):
-        # state vector
-        y1 = initial_state[0]
-        # y2 = initial_state[1] # not directly involved in ode system
-
-        dev_y1 = -(U + U**2 * a) * y1
-        dev_y2 = U * y1 * b
-        return np.array([dev_y1, dev_y2], dtype='float64')
-
     ode = scp.ode(state_model)
-    ode.set_integrator('lsoda', nsteps=3000)
-    ode.set_initial_value(initial_state, t=0.0)  # initial time always 0
-    return ode.integrate(ode.t + time_interval)
+    ode.set_integrator('dopri5')
+    ode.set_f_params([U, a, b])
+    ode.set_initial_value(initial_state)  # initial time is zero by default
+    final_state = ode.integrate(ode.t + time_interval)
+    return final_state
