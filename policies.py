09c4a7cb86d0b0268594c512d9fa368012b90d71
Ilya Orson <ilya.orson@gmail.com>
2018-09-24 23:37:42 -0500

    change control interval to (0,5)

--- policies.py
+++ policies.py
@@ -3,10 +3,6 @@ import torch.nn.functional as F
 
 
 class NeuralNetwork(nn.Module):
-    """
-    Feed-forward neural network that takes in the states.
-    Outputs mean and std of an action (exploration-explotation).
-    """
 
     def __init__(self, hidden_size, num_inputs, num_outputs):
         super(NeuralNetwork, self).__init__()
@@ -18,19 +14,17 @@ class NeuralNetwork(nn.Module):
         x = inputs
         x = F.relu(self.linear1(x))
         x = F.relu(self.linear2(x))
-        # mu = F.relu6(self.linear3(x))
-        mu = 6 * F.sigmoid(self.linear3(x))
+        mu = 5 * F.sigmoid(self.linear3(x))
         return mu
 
 
 class LinearRegression(nn.Module):
 
     def __init__(self, input_dim, output_dim):
-
         super(LinearRegression, self).__init__()
         self.linear = nn.Linear(input_dim, output_dim)
 
     def forward(self, x):
         x = self.linear(x)
-        out = 6 * F.sigmoid(x)
+        out = 5 * F.sigmoid(x)
         return out
