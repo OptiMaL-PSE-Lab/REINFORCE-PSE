4b3997ea7f24d299ac729c5b340d8f9197ddba8c
Ilya Orson <ilya.orson@gmail.com>
2019-02-03 10:05:48 -0600

    WIP add complex model

--- integrator.py
+++ integrator.py
@@ -6,7 +6,6 @@ import scipy.integrate as scp
 
 # base class for what we we expect from any ODE model
 class ODEModel(object):
-
     def __init__(self, parameters, controls_dims, state_dims, integrator="dopri5"):
         self.parameters = parameters
         self.controls_dims = controls_dims
@@ -14,13 +13,27 @@ class ODEModel(object):
         self.ode = scp.ode(self.system).set_integrator(integrator)
 
     def _check_dims(self, controls, state):
-        # TODO: allow non-iterabe controls and states
-        assert self.controls_dims == len(controls), f"This model expects {self.controls_dims} controls!"
-        assert self.state_dims == len(state), f"This model expects {self.state_dims} controls!"
+        try:
+            iter(controls)
+            iter(state)
+        except TypeError:
+            raise TypeError(
+                "Please use containers for controls and states: value --> [value]."
+            )
+        else:
+            assert self.controls_dims == len(
+                controls
+            ), f"This model expects {self.controls_dims} controls!"
+            assert self.state_dims == len(
+                state
+            ), f"This model expects {self.state_dims} controls!"
 
-    @staticmethod
-    def system(t, state, f_args):  # f_args should be a concatenation of parameters and controls (parameters first always!)
-        raise NotImplementedError("The function defining the dynamical system modeled must be specified!")
+    # f_args should be a concatenation of parameters and controls (parameters first always!)
+    @staticmethod  # static required to pass it as argument to scipy ode integrator
+    def system(t, state, f_args):
+        raise NotImplementedError(
+            "The function defining the dynamical system modeled must be specified!"
+        )
 
     def integrate(self, controls, initial_state, integration_time, initial_time=0.0):
         self._check_dims(controls, initial_state)
@@ -32,22 +45,40 @@ class ODEModel(object):
 
 
 class SimpleModel(ODEModel):
-
-    def __init__(self, parameters, controls_dims, state_dims):
-        super().__init__(parameters, controls_dims, state_dims)
+    def __init__(self, parameters):
+        control_dims = 1
+        state_dims = 2
+        super().__init__(parameters, control_dims, state_dims)
 
     @staticmethod
     def system(t, state, f_args):
+
         a, b, U = f_args
         y1, _ = state
 
-        y1_prime = -(U + U**2 * a) * y1
-        y2_prime = U * y1 * b
+        y1_prime = -(U + a * U ** 2) * y1
+        y2_prime = b * U * y1
 
         return [y1_prime, y2_prime]
 
-S = SimpleModel([0.5,1.0], 1, 2)
-S.integrate([5.0], (1,0), 0.5)
+
+class ComplexModel(ODEModel):
+    def __init__(self, parameters):
+        control_dims = 2
+        state_dims = 2
+        super().__init__(parameters, control_dims, state_dims)
+
+    @staticmethod
+    def system(t, state, f_args):
+
+        a, b, U1, U2 = f_args
+        y1, y2 = state
+
+        y1_prime = -(U1 + a * U2 ** 2) * y1 + 0.1 * U2 ** 2 / (y1 + y2)
+        y2_prime = b * U1 * y1 - 0.7 * U2 * y1
+
+        return [y1_prime, y2_prime]
+
 
 # def simple_system(t, state, f_args):
 
